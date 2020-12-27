2a24b101004dff9602bf87ba3579fce78c88c7a9
timlin <timlin1972@gmail.com>
2020-12-27 09:58:50 +0800

    server update

--- server/lib/resource.js
+++ server/lib/resource.js
@@ -1,18 +1,19 @@
 module.exports = {
-  en: {
-    translation: {
-      'Initialized': 'Initialized',
-      'Run': 'Run',
-      'http server is listening at port': 'http server is listening at port',
-      'graphql server is listening at port': 'graphql server is listening at port',
-    }
-  },
+  // en: {
+  //   translation: {
+  //   }
+  // },
   zh: {
     translation: {
       'Initialized': '初始化完成',
       'Run': '執行',
       'http server is listening at port': 'http 伺服器監聽於以下埠號', 
       'graphql server is listening at port': 'graphql 伺服器監聽於以下埠號', 
+      'Item is not found': '項目未找到',
+      'Send WOL to': '送 WOL 到',
+      'Host': '主機',
+      'is alive': '上線',
+      'is dead': '離線',
     }
   },
 };
