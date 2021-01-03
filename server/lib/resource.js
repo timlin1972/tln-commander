022e43725f02ec7f2d66ab9f797dc8c39a31f079
timlin <timlin1972@gmail.com>
2021-01-03 17:10:10 +0800

    Add graphql subscription

--- server/lib/resource.js
+++ server/lib/resource.js
@@ -9,7 +9,7 @@ module.exports = {
       'Run': '執行',
       'http server is listening at port': 'http 伺服器監聽於以下埠號', 
       'https server is listening at port': 'https 伺服器監聽於以下埠號', 
-      'graphql server is listening at port': 'graphql 伺服器監聽於以下埠號', 
+      'graphql server is listening at': 'graphql 伺服器監聽於以下', 
       'Item is not found': '項目未找到',
       'Send WOL to': '送 WOL 到',
       'Host': '主機',
