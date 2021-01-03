3f5cc9a1765cdf6af51a2ee43912941bfecf331c
timlin <timlin1972@gmail.com>
2021-01-03 23:09:47 +0800

    Add autologout]

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import * as actions from '../src/state/actions';
+
+import Autologout from '../tln-autologout';
+// import Header from './Header/Header';
+// import Content from './Content/Content';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+}));
 
 const Main = (props) => {
-  const { t } = useTranslation();
+  const classes = useStyles();
+  const { user, logoutUser } = props;
 
-  return <h1>{t('App')}</h1>;
+  return (
+    <div className={classes.root}>
+      <Autologout logoutTimeout={user.logoutTimeout} logoutUser={logoutUser} debug={true} />
+      <CssBaseline />
+      {/* <Header />
+      <Content /> */}
+    </div>
+  );
 };
 
-export default (Main);
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    logoutUser: bindActionCreators(actions.logoutUser, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
